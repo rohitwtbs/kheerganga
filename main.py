a3f9468f2c1bd70826726d0c3a11e2702edef7c5
rohit kumar <12rohit4@gmail.com>
2025-12-24 21:51:38 +0530

    websocket variable should be of type webSocket which is imported from fastapi

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from fastapi import FastAPI , websocket , websocketdisconnect
+from fastapi import FastAPI , WebSocket , websocketdisconnect
 from typing import List
 
 class ConnectionManager:
     def __init__(self):
         pass
 
-    def connect(self, websocket: websocket):
+    def connect(self, websocket: WebSocket):
         pass
 
-    def disconnect(self, websocket:websocket):
+    def disconnect(self, websocket: WebSocket):
         pass
 
     def broadcast(self, message:str):
